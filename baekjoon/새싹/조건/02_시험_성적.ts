8849c7bd0d83f73f2bffc910479f5b14041a392f
perlpark <hello@jinjoo.me>
2023-01-03 21:48:38 +0900

    baekjoon > 새싹 > 조건 > 시험 성적 완료

---
+++ baekjoon/새싹/조건/02_시험_성적.ts
@@ -0,0 +1,33 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/1330
+ */
+
+{
+  const readline = require('readline');
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdin,
+  });
+
+  rl.on('line', (line: string) => {
+    const score = parseInt(line);
+    if (score > 89) {
+      console.log('A');
+    } else if (score > 79) {
+      console.log('B');
+    } else if (score > 69) {
+      console.log('C');
+    } else if (score > 59) {
+      console.log('D');
+    } else {
+      console.log('F');
+    }
+    rl.close();
+  });
+
+  rl.on('close', () => {
+    process.exit();
+  });
+}
