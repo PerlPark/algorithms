b0f5afede8e9065362d73a0ba4ec8984c1b9f681
perlpark <hello@jinjoo.me>
2023-01-07 23:51:15 +0900

    baekjoon > 새싹 > 반복 > 구구단 완료

---
+++ baekjoon/새싹/반복/05_구구단.js
@@ -0,0 +1,13 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/10952
+ */
+
+{
+  const number = require('fs').readFileSync('/dev/stdin').toString().trim();
+  let result = '';
+  for (let i = 1; i < 10; i++) {
+    result += `${number} * ${i} = ${+number * i}\n`;
+  }
+  console.log(result);
+}
