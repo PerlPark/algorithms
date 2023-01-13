7e37d325baa0bd3cec49e7ed5bff5cd09d6d248d
perlpark <hello@jinjoo.me>
2023-01-13 14:49:54 +0900

    baekjoon > 새싹 > 문자열 > 아스키 코드 완료

---
+++ baekjoon/새싹/문자열/01_아스키_코드.js
@@ -0,0 +1,7 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/11654
+ */
+
+const string = require('fs').readFileSync('test.txt').toString().trim();
+console.log(string.charCodeAt());
