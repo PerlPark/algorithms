338f04256002f9269651eef17f3cee6007c6f411
perlpark <hello@jinjoo.me>
2023-01-14 14:41:57 +0900

    baekjoon > 새싹 > 문자열 > 단어 길이 재기 완료

---
+++ baekjoon/새싹/문자열/02_단어_길이_재기.js
@@ -0,0 +1,7 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/11654
+ */
+
+const string = require('fs').readFileSync('/dev/stdin').toString().trim();
+console.log(string.length);
