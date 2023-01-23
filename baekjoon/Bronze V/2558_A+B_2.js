065489d28b4917daabb4f1495a1aab93bff128fa
perlpark <hello@jinjoo.me>
2023-01-23 14:11:16 +0900

    baekjoon > Bronze V > A+B - 2 완료

---
+++ baekjoon/Bronze V/2558_A+B_2.js
@@ -0,0 +1,7 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/2558
+ */
+
+const [a, b] = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\n/);
+console.log(+a + +b);
