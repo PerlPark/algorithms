68f79e40bd100e3e7373a42ebe243a19d4daecf5
perlpark <hello@jinjoo.me>
2023-01-24 10:53:08 +0900

    baekjoon > Bronze V > 마이크로소프트 로고 완료

---
+++ baekjoon/Bronze V/5338_마이크로소프트_로고.js
@@ -0,0 +1,10 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/5338
+ */
+
+console.log("       _.-;;-._");
+console.log("'-..-'|   ||   |");
+console.log("'-..-'|_.-;;-._|");
+console.log("'-..-'|   ||   |");
+console.log("'-..-'|_.-''-._|");
