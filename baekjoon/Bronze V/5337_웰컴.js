6f41ad4c2c7238825904c44b96567ccb0224743b
perlpark <hello@jinjoo.me>
2023-01-24 10:49:56 +0900

    baekjoon > Bronze V > 웰컴 완료

---
+++ baekjoon/Bronze V/5337_웰컴.js
@@ -0,0 +1,8 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/5337
+ */
+
+console.log('.  .   .');
+console.log('|  | _ | _. _ ._ _  _');
+console.log('|/\\|(/.|(_.(_)[ | )(/.');
