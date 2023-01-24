35c7c508ccb121090ce46bde7a282c1e354d7d5c
perlpark <hello@jinjoo.me>
2023-01-24 10:58:18 +0900

    baekjoon > Bronze V > 콜센터 완료

---
+++ baekjoon/Bronze V/5339_콜센터.js
@@ -0,0 +1,15 @@
+/**
+ *  BAEKJOON ONLINE JUDGE
+ *  https://www.acmicpc.net/problem/5339
+ */
+
+console.log('     /~\\');
+console.log('    ( oo|');
+console.log('    _\\=/_');
+console.log('   /  _  \\');
+console.log('  //|/.\\|\\\\');
+console.log(' ||  \\ /  ||');
+console.log('============');
+console.log('|          |');
+console.log('|          |');
+console.log('|          |');
